76f71b97e4a0f99ff7810e473a7b985dd2b6ea5f
relmaazouz <relmaazouz@protonmail.com>
2024-07-29 00:39:50 +0200

    upload drug prescription front and backend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import chatbot_diagnosis, drug_identification
+from .views import chatbot_diagnosis, drug_identification, upload_drug_prescription_view
 
 urlpatterns = [
     path('diagnosis/', chatbot_diagnosis, name='chatbot_diagnosis'),
     path('drug/', drug_identification, name='drug_identification'),
+    path('upload_drug_prescription/', upload_drug_prescription_view.as_view(), name="upload_drug_prescription"),
 ]
